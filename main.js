2ab68d537c684d069e5e2865cc38f5896bc01951
JacobScott1 <S1615275@southstaffs.ac.uk>
2017-04-27 10:55:25 +0100

    udate main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+//Create our 'main' state that will contain the game
+var mainStage = {
+    preload: function() {
+        //this function will be executed at the begining
+        //thats where we load the images and the sound
+    },
+    
+    create: function() {
+        //this function is called after the preload functioin
+        //here we setup the game, display sprites, etc...
+    },
+    
+    update: function() {
+    //this function is called 60 times per second
+        //it contains the games logic
+    },
+};
+
+//initialise Phaser, and create a 400px x 490px game
+var game = new Phaser.Game(400, 490);
+
+//add the 'mainState' and call it 'main
+game.state.add('main', mainState);
+
+//start the state to actually start the game
+game.state.start('main');
